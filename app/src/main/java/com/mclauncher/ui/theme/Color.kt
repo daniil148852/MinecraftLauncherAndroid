3e13ac3bf7fd9d6c2aababf9aeef86d827d45e0a
daniil148852 <daniildan850@gmail.com>
2026-02-02 18:58:52 +0100

    Update Color.kt

---
+++ app/src/main/java/com/mclauncher/ui/theme/Color.kt
@@ -0,0 +1,94 @@
+package com.mclauncher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors - Minecraft Grass Green
+val Green10 = Color(0xFF002200)
+val Green20 = Color(0xFF004400)
+val Green30 = Color(0xFF006600)
+val Green40 = Color(0xFF2E7D32)
+val Green50 = Color(0xFF4CAF50)
+val Green60 = Color(0xFF66BB6A)
+val Green70 = Color(0xFF81C784)
+val Green80 = Color(0xFFA5D6A7)
+val Green90 = Color(0xFFC8E6C9)
+val Green95 = Color(0xFFE8F5E9)
+val Green99 = Color(0xFFF1F8E9)
+
+// Secondary Colors - Minecraft Dirt Brown
+val Brown10 = Color(0xFF1A1207)
+val Brown20 = Color(0xFF33240E)
+val Brown30 = Color(0xFF4D3615)
+val Brown40 = Color(0xFF66481C)
+val Brown50 = Color(0xFF8D6E63)
+val Brown60 = Color(0xFFA1887F)
+val Brown70 = Color(0xFFBCAAA4)
+val Brown80 = Color(0xFFD7CCC8)
+val Brown90 = Color(0xFFEFEBE9)
+val Brown95 = Color(0xFFFAF8F7)
+
+// Tertiary Colors - Minecraft Water Blue
+val Blue10 = Color(0xFF001F33)
+val Blue20 = Color(0xFF003D66)
+val Blue30 = Color(0xFF005C99)
+val Blue40 = Color(0xFF0077B6)
+val Blue50 = Color(0xFF0096C7)
+val Blue60 = Color(0xFF00B4D8)
+val Blue70 = Color(0xFF48CAE4)
+val Blue80 = Color(0xFF90E0EF)
+val Blue90 = Color(0xFFCAF0F8)
+val Blue95 = Color(0xFFE0F7FA)
+
+// Error Colors - Minecraft Lava Red
+val Red10 = Color(0xFF330000)
+val Red20 = Color(0xFF660000)
+val Red30 = Color(0xFF990000)
+val Red40 = Color(0xFFB71C1C)
+val Red50 = Color(0xFFD32F2F)
+val Red60 = Color(0xFFE53935)
+val Red70 = Color(0xFFEF5350)
+val Red80 = Color(0xFFEF9A9A)
+val Red90 = Color(0xFFFFCDD2)
+val Red95 = Color(0xFFFFEBEE)
+
+// Neutral Colors - Stone Gray
+val Gray0 = Color(0xFF000000)
+val Gray10 = Color(0xFF1A1A1A)
+val Gray20 = Color(0xFF333333)
+val Gray30 = Color(0xFF4D4D4D)
+val Gray40 = Color(0xFF666666)
+val Gray50 = Color(0xFF808080)
+val Gray60 = Color(0xFF999999)
+val Gray70 = Color(0xFFB3B3B3)
+val Gray80 = Color(0xFFCCCCCC)
+val Gray90 = Color(0xFFE6E6E6)
+val Gray95 = Color(0xFFF2F2F2)
+val Gray99 = Color(0xFFFAFAFA)
+val Gray100 = Color(0xFFFFFFFF)
+
+// Special Minecraft Colors
+val MinecraftGreen = Color(0xFF5D9B47)
+val MinecraftBrown = Color(0xFF8B5A2B)
+val MinecraftStone = Color(0xFF7F7F7F)
+val MinecraftWood = Color(0xFFBC9862)
+val MinecraftDiamond = Color(0xFF4AEDD9)
+val MinecraftGold = Color(0xFFDDAA00)
+val MinecraftIron = Color(0xFFD8D8D8)
+val MinecraftRedstone = Color(0xFFAA0000)
+val MinecraftLapis = Color(0xFF345EC3)
+val MinecraftEmerald = Color(0xFF17DD62)
+val MinecraftNetherPortal = Color(0xFF9B4DCA)
+val MinecraftEndPortal = Color(0xFF0E1111)
+val MinecraftEnchant = Color(0xFFB388FF)
+
+// Surface Colors
+val SurfaceDark = Color(0xFF121212)
+val SurfaceDarkElevated = Color(0xFF1E1E1E)
+val SurfaceLight = Color(0xFFFFFBFE)
+val SurfaceLightDim = Color(0xFFF5F5F5)
+
+// Gradient Colors
+val GradientStartDark = Color(0xFF1B3D1B)
+val GradientEndDark = Color(0xFF0D1F0D)
+val GradientStartLight = Color(0xFFE8F5E9)
+val GradientEndLight = Color(0xFFC8E6C9)
